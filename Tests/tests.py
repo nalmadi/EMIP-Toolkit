e112594cbe7e8560277956b8159ff2b69c6f6fb8
AidanSweeny <apswee22@colby.edu>
2021-12-19 12:15:28 -0800

    Final url changes

--- Tests/tests.py
+++ Tests/tests.py
@@ -75,23 +75,18 @@ def test_draw_aoi():
     rect_img = tk.draw_aoi(aoi, abs_file_path, "")
     assert type(rect_img) == Image.Image
 
-
-
 def test_add_tokens_to_AOI():
-    file_path = './emip_dataset/EMIP_DataCollection_Materials/emip_stimulus_programs/'
+    file_path = '../emip_dataset/EMIP_DataCollection_Materials/emip_stimulus_programs/'
     aois_raw = tk.find_aoi(image= "vehicle_java.jpg" , image_path = './emip_dataset/stimuli/' )
     aois_tokens = tk.add_tokens_to_AOIs(file_path,aois_raw)
     assert type(aois_tokens) == pd.core.frame.DataFrame
     assert aois_tokens["image"][1] == "vehicle_java.jpg"
     assert len(aois_tokens["token"]) == 112
 
-
-
-
 def test_add_srcml_to_AOIs():
     file_path = '../emip_dataset/EMIP_DataCollection_Materials/emip_stimulus_programs/'
     aois_raw = tk.find_aoi(image= "vehicle_java.jpg" , image_path = './emip_dataset/stimuli/' )
-    srcML_path = './datasets/EMIP2021/'
+    srcML_path = '../datasets/EMIP2021/'
     AOIs_wSRCs= tk.add_srcml_to_AOIs(aois_raw, srcML_path)
     
     assert type(AOIs_wSRCs) == pd.core.frame.DataFrame
